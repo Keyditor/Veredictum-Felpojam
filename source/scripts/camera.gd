835c536847e0798abc6b4fd910bcf5530890b1d8
Arthur <lourencoarthur224@gmail.com>
2026-02-20 23:27:14 -0300

    Começo da implementação das caixas de diálogo

--- source/scripts/camera.gd
+++ source/scripts/camera.gd
@@ -9,7 +9,7 @@ func start_shake(intensity, duration):
 	
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass
+	Dialogic.start("intro")
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
